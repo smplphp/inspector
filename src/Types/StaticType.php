b543f0ddb6519733e6ebd074b007074338999a53
Ollie Read <code@ollie.codes>
2022-04-09 23:46:55 +0100

    feat(types): Add self and static types

--- src/Types/StaticType.php
+++ src/Types/StaticType.php
@@ -4,7 +4,49 @@ declare(strict_types=1);
 
 namespace Smpl\Inspector\Types;
 
-class StaticType extends ClassType
-{
+use Smpl\Inspector\Contracts\Type;
+use Smpl\Inspector\Factories\TypeFactory;
 
+class StaticType extends BaseType
+{
+    /**
+     * @var \Smpl\Inspector\Types\ClassType
+     */
+    private ClassType $baseType;
+
+    public function __construct(ClassType $class)
+    {
+        $this->baseType = $class;
+    }
+
+    public function getName(): string
+    {
+        return 'static';
+    }
+
+    public function isPrimitive(): bool
+    {
+        return false;
+    }
+
+    public function isBuiltin(): bool
+    {
+        return false;
+    }
+
+    /** @infection-ignore-all  */
+    public function getBaseType(): Type
+    {
+        return $this->baseType;
+    }
+
+    public function matches(mixed $value): bool
+    {
+        return $this->getBaseType()->matches($value);
+    }
+
+    public function accepts(Type|string $type): bool
+    {
+        return $this->getBaseType()->accepts($type);
+    }
 }
